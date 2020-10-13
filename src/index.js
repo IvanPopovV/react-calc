8e17ec68aac2302efd2ec7853694881c45bf5e66
Ivan <62757881+IvanPopovV@users.noreply.github.com>
2020-10-13 16:59:26 +0300

    react-calc-money

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './normalize.css';
+import './style.css';
+
+// const Header = ({text}) => <h1 id="hi">{text}</h1>;
+
+// const Element = <p>Параграф</p>;
+
+// class Main extends React.Component {
+//   render() {
+//     return (
+//       <div className="main">
+//         <Header text={'свойство текст' + this.props.count} />
+//         {Element}
+//       </div>
+//     )
+//   }
+// }
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    {/* <Main count={5} /> */}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
